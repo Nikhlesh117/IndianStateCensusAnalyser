29493d9a3ff61b213eaf0a2fdc5fc178744c1782
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-26 18:05:11 +0530

    Added: Load State Census Csv

---
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian State Census Analyser Problem");
+
+            string filePath = "D:\\c#\\257\\IndianStatesCensusAnalyserProblem\\IndianStatesCensusAnalyserProblem\\StaeCensus.csv";
+            StateCensusAnalyser analyser = new StateCensusAnalyser(filePath);
+            int numberOfRecords = analyser.LoadDataFromCSV();
+            Console.WriteLine("Number of records in the file: " + numberOfRecords);
+        }
+    }
+}
